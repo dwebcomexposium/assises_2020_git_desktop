10be451bb143be56543e94e7685e32e00e13dacb
Flore Chasles <flore.chasles@gmail.com>
2019-11-21 16:22:29 +0100

    First commit assises 2020

--- js/modules/surcharge.js
+++ js/modules/surcharge.js
@@ -1,57 +1,7 @@
-/** Cookie for header **/
-  function setCookie(name, value, days) {
-    var d = new Date();
-    d.setTime(d.getTime() + (days * 24 * 60 * 60 * 1000));
-    var expires = "expires="+ d.toUTCString();
-    document.cookie = name + "=" + value + ";" + expires + ";path=/";
-  }
-
-  function getCookie(name) {
-    var cookieName = name + "=";
-    var decodedCookie = decodeURIComponent(document.cookie);
-    var ca = decodedCookie.split(';');
-    for(var i = 0; i <ca.length; i++) {
-      var c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(cookieName) == 0) {
-        return c.substring(cookieName.length, c.length);
-      }
-    }
-    return "";
-  }
-
 $(document).ready(function(){
   /** Slick init **/
   $('.videos').slick({
     dots: true,
     arrows: true,
   });
-
-  /** Height zone1 + position **/
-  var $articleIntro = $('.article-intro');
-  var $mainTitle = $('.at-main-title');
-  var $articleTitle = $('.article-title');
-  if ($articleIntro.length > 0 && $mainTitle.length > 0) {
-    $articleIntro.css('top', $mainTitle.position().top + $mainTitle.height() + 20 + 'px');
-    var headerHeight = $articleIntro.height() + $mainTitle.height() + $('.quicklinks-navigation').height() + $('.breadcrumb-nav .inside').height() + 60;
-    if (headerHeight > $('#zone1').height()) {
-      $('#zone1, .article-title img').css('height', headerHeight);
-    }
-  } else if ($articleTitle.length > 0 && $mainTitle.length > 0) {
-    $articleTitle.css('top', $mainTitle.position().top + $mainTitle.height() + 20 + 'px');
-    var headerHeight = $articleTitle.height() + $mainTitle.height() + $('.quicklinks-navigation').height() + $('.breadcrumb-nav .inside').height() + 60;
-    if (headerHeight > $('#zone1').height()) {
-      $('#zone1, .article-title img').css('height', headerHeight);
-    }
-  }
-
-  /** Cookie for header **/
-  var cookieName = getCookie("header_cookie");
-  if (cookieName === '') {
-    setCookie('header_cookie', 'header_cookie', 365);
-  } else {
-    $('.site-banner').addClass('is-slim');
-  }
 });
