903af2473429691e01c293d4a7bdc11084166e19
Flore Chasles <flore.chasles@gmail.com>
2020-01-21 16:48:39 +0100

    resize zone1 for mobile

--- js/modules/surcharge.js
+++ js/modules/surcharge.js
@@ -6,6 +6,25 @@ $(document).ready(function(){
     mobileFirst: true
   });
 
+  /** Height zone1 + position **/
+  var $articleIntro = $('.article-intro');
+  var $mainTitle = $('.at-main-title');
+  var $articleTitle = $('.article-title');
+
+  if ($articleIntro.length > 0 && $mainTitle.length > 0) {
+    $articleIntro.css('top', $mainTitle.position().top + $mainTitle.height() + 20 + 'px');
+    var headerHeight = $articleIntro.height() + $mainTitle.height() + $('.quicklinks-navigation').height() + $('.breadcrumb-nav .inside').height() + 60;
+    if (headerHeight > $('#zone1').height()) {
+      $('#zone1, .article-title img').css('height', headerHeight);
+    }
+  } else if ($articleTitle.length > 0 && $mainTitle.length > 0) {
+    $articleTitle.css('top', $mainTitle.position().top + $mainTitle.height() + 20 + 'px');
+    var headerHeight = $articleTitle.height() + $mainTitle.height() + $('.quicklinks-navigation').height() + $('.breadcrumb-nav .inside').height() + 60;
+    if (headerHeight > $('#zone1').height()) {
+      $('#zone1, .article-title img').css('height', headerHeight);
+    }
+  }
+
   /** ScroolTop **/
   function scroll_to_top(div) {
     $(div).click(function() {
